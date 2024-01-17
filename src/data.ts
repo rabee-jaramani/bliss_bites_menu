8764bfc555b78caa232d057148d4a663f085a31d
Rabee Aljaramani <rabeejermani@gmail.com>
2024-01-17 18:50:44 +0400

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,13 +1,20 @@
 import placeholder from './images/placeholder.webp'
 import { FullMenu_Interface } from './interfaces';
 export const food_full_menu = [
-
+// =CONCATENATE("{",B3,"'",C3,"'",CHAR(44),D3,"'",E3,"'",CHAR(44),F3,"'",G3,"'",CHAR(44),"image:placeholder",CHAR(44),H3,"[","'",I3,"']}",CHAR(44))
         {
           categoryName: 'Appetizers',
           items: [
-            { itemName: 'Bruschetta', price: '58', description: 'xxdescription', image: placeholder,addons:['1. add on a','2. add on b','3. add on c','4. add on d'] },
-            { itemName: 'Chicken Wings', price: ' 10', description: 'descripti ondesc riptiondescrip tiond escripti ondescri ptio ndesc ription descriptiondescriptionv descriptiondescription', image: placeholder },
-            { itemName: 'Chicken Wings', price: ' 10', description: 'descripti ondesc riptiondescrip tiond escripti ondescri ptio ndesc ription descriptiondescriptionv descriptiondescription', image: placeholder },
+            {itemName:'Bruschetta',price:'55',description:'descripti ondesc riptiondescrip tiond escripti ondescri ptio ndesc',image:placeholder,addons:['1. add on a,2. add on b,3. add on c,4. add on d']},
+            {itemName:'Brchetta',price:'55',description:'descripti ondesc riptiondescrip tiond escripti ondescri ptio ndesc',image:placeholder,addons:['1. add on a,2. add on b,3. add on c,4. add on d']},
+            {itemName:'Bruscgfchetta',price:'55',description:'descripti ondesc riptiondescrip tiond escripti ondescri ptio ndesc',image:placeholder,addons:['1. add on a,2. add on b,3. add on c,4. add on d']},
+            {itemName:'Bruscgga',price:'55',description:'descripti ondesc riptiondescrip tiond escripti ondescri ptio ndesc',image:placeholder,addons:['1. add on a,2. add on b,3. add on c,4. add on d']},
+            {itemName:'Bhetta',price:'55',description:'descripti ondesc riptiondescrip tiond escripti ondescri ptio ndesc',image:placeholder,addons:['1. add on a,2. add on b,3. add on c,4. add on d']},
+            {itemName:'Brusch6etta',price:'55',description:'descripti ondesc riptiondescrip tiond escripti ondescri ptio ndesc',image:placeholder,addons:['1. add on a,2. add on b,3. add on c,4. add on d']},
+            {itemName:'Brus4chetta',price:'55',description:'descripti ondesc riptiondescrip tiond escripti ondescri ptio ndesc',image:placeholder,addons:['1. add on a,2. add on b,3. add on c,4. add on d']},
+            {itemName:'Bru3schetta',price:'55',description:'descripti ondesc riptiondescrip tiond escripti ondescri ptio ndesc',image:placeholder,addons:['1. add on a,2. add on b,3. add on c,4. add on d']},
+            {itemName:'Brusc0hetta',price:'55',description:'descripti ondesc riptiondescrip tiond escripti ondescri ptio ndesc',image:placeholder,addons:['1. add on a,2. add on b,3. add on c,4. add on d']},
+            
           ],
         },
         {
@@ -15,10 +22,10 @@ export const food_full_menu = [
           items: [
             { itemName: 'Spaghetti Bolognese', price: ' 12', description: 'Description this is a lorem text and will change it soon', image: placeholder },
             { itemName: 'Grilled Salmon', price: ' 15', description: 'Description this is a lorem text and will change it soon', image: placeholder },
-            { itemName: 'Spaghetti Bolognese', price: ' 12', description: 'Description this is a lorem text and will change it soon', image: placeholder },
-            { itemName: 'Grilled Salmon', price: ' 15', description: 'Description this is a lorem text and will change it soon', image: placeholder },
-            { itemName: 'Spaghetti Bolognese', price: ' 12', description: 'Description this is a lorem text and will change it soon', image: placeholder },
-            { itemName: 'Grilled Salmon', price: ' 15', description: 'Description this is a lorem text and will change it soon', image: placeholder },
+            { itemName: 'Spaghffetti Bolognese', price: ' 12', description: 'Description this is a lorem text and will change it soon', image: placeholder },
+            { itemName: 'Grillefd Salmon', price: ' 15', description: 'Description this is a lorem text and will change it soon', image: placeholder },
+            { itemName: 'Spaghetdsti Bolognese', price: ' 12', description: 'Description this is a lorem text and will change it soon', image: placeholder },
+            { itemName: 'Grill4ed Salmon', price: ' 15', description: 'Description this is a lorem text and will change it soon', image: placeholder },
           ],
         },
         {
